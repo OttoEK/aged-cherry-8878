6e8dadb2931b37133d726d47342974f06b739c0f
ottoek <otto.e.kog@gmail.com>
2016-11-22 13:41:47 -0300

    first commit

---
+++ client/app/components/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { TaskService } from '../../services/tasks.service';
+import { Task } from '../../../Task';
+
+@Component({
+  moduleId: module.id,
+  selector: 'tasks',
+  templateUrl: 'tasks.component.html'
+})
+
+export class TasksComponent {
+  tasks: Task[];
+
+  constructor(private taskService : TaskService){
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
+}
